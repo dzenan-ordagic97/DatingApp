3952a52e96ca278dac875b624266bffb95c98c43
Dženan Ordagić <dzenan.ordagic@edu.fit.ba>
2020-09-28 15:32:59 +0200

    initial commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController:ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values= await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value= await _context.Values.Where(x=>x.Id==id).FirstOrDefaultAsync();
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "ChillyRRR", "Cool", "Mild", "Warmw", "BalmyTest", "Hot", "Sweltering", "Scorching"
+        };
+
+        // private readonly ILogger<WeatherForecastController> _logger;
+
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values=_context.Values.ToList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value=_context.Values.Where(x=>x.Id==id).FirstOrDefault();
+            return Ok(value);
+        }
+    }
+}
